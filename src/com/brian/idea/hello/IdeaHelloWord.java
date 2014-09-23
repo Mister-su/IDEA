28ccbb45b38115bfb7dd601dbe7be521cf9ebc76
Mister-su <1197003477@qq.com>
2014-09-23 12:21:12 +0800

    Initial commit

---
+++ src/com/brian/idea/hello/IdeaHelloWord.java
@@ -0,0 +1,10 @@
+package com.brian.idea.hello;
+
+/**
+ * Created by Mike on 2014/9/23.
+ */
+public class IdeaHelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello IDEA");
+    }
+}
